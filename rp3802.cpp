b0835736b95ea74425f3a433ab2bed2433f4e2d0
hyano <1297631+hyano@users.noreply.github.com>
2025-05-18 11:30:00 +0900

    fixed UART baudrate.

--- rp3802.cpp
+++ rp3802.cpp
@@ -63,7 +63,7 @@
 
 
 #define UART_ID                 (uart0)
-#define BAUD_RATE               (312500)
+#define BAUD_RATE               (31250)
 #define UART_TX_PIN             (0)
 #define UART_RX_PIN             (1)
 
